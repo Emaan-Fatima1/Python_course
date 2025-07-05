86b4fac44f38200b558fad4a4658ca7048cc937a
Emaan-Fatima1 <emaan.fatima122006@gmail.com>
2025-07-05 19:55:51 +0500

    Python tuples and sets

---
+++ Tuples and Sets.py
@@ -0,0 +1,27 @@
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple)
+
+# thistuple = ("apple", "banana", "cherry")
+# print(len(thistuple))
+
+# thistuple = ("apple",)
+# print(type(thistuple))
+
+# #NOT a tuple
+# thistuple = ("apple")
+# print(type(thistuple))
+
+# thistuple = ("apple", "banana", "cherry")
+# for x in thistuple:
+#   print(x)
+
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print[i]
+  print(thistuple[i])
+
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
